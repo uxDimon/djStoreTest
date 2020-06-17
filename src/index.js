ee63f7d68521735422a4f9b0377db8975342ed80
Shelkov Dmitriy <bobyorrr@gmail.com>
2020-06-17 11:36:51 +0300

    readiness

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
+// Подключает scss
 import "./styles.scss";
-console.log("hello world!");
+
+// Подключает слайдер
+import "../node_modules/tiny-slider/src/tiny-slider.scss";
+import { tns } from "tiny-slider/src/tiny-slider";
+
+// настройки слайдера
+var slider1 = tns({
+	container: ".my-slider",
+	items: 1,
+	mode: "gallery",
+	prevButton: ".b-prev",
+	nextButton: ".b-next",
+	nav: false,
+});
+var slider2 = tns({
+	container: ".my-slider2",
+	items: 2,
+	prevButton: ".b-prev",
+	nextButton: ".b-next",
+	nav: false,
+});
+
+// скрывает меню при скроле
+const header = document.querySelector(".header");
+let scroll = 0;
+
+document.addEventListener("scroll", () => {
+	if (window.pageYOffset > scroll) {
+		header.classList.remove("opa");
+		scroll = window.pageYOffset;
+	}
+	if (window.pageYOffset < scroll) {
+		header.classList.add("opa");
+		scroll = window.pageYOffset;
+	}
+});
